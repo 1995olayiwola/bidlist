5da01ae8b8701dc80784370560041f558154f73a
olayiwola abdur-rahmon <olayiwolarahmon84@gmail.com>
2024-05-29 17:45:13 +0100

    forth commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,40 +9,40 @@ const Home = () => {
   
     <div className='container'>
  <Header/>
-<div>
+<div className='bidding_hub'>
   <h1>BIDDING HUB</h1>
   <p>
 HOME OF SITE PREP JOB OPPORTUNITIES
 </p>
 <p>Unleash the Power of Bidding - Connecting Top Contractors with Lucrative Projects!</p>
 </div>
-<div>
+<div className='our_story'>
   <h1>Our Story</h1>
   <p>Welcome to BidConnect, where bidding meets brilliance! We are the ultimate platform for site prep contractors and clients seeking top-notch expertise.
 </p>
 <p>With a wealth of experience in the bidding business, we excel in connecting verifiable contractors with real-world clients for construction projects.</p>
 <p>Join us to revolutionize the way projects are handled, ensuring excellence and efficiency every step of the way!</p>
 </div>
-<div>
+<div className='trilled'>
   <h1>Thrilled</h1>
   <p>Emily Johnson (COMPANY NAME)</p>
   <p>Incredible service, saved me time and money!
 </p>
 </div>
-<div>
+<div className='company_name'>
   <h1>(Company Name)</h1>
   <p>Best decision I made for my project, highly recommend!</p>
 </div>
-<div>
+<div className='meet_us'>
   <h1>Meet Us</h1>
   <h3>David Ceo, Sophia Coo, Alex Cto, Linda Cfo</h3>
 </div>
-<div>
+<div className='instant_matches'>
   <h1>Instant Matches.</h1>
   <p>Our cutting-edge technology ensures instant and accurate matches between contractors and clients, saving you time and hassle. Contact us to get the link to you to the best and profitable site prep contract.</p>
 </div>
-<div>
-  <h1>Client Success</h1>
+<div className="client_success">
+  <h1 >Client Success</h1>
   <h3>Join our platform and become part of our success stories, where dreams turn into reality with the right contractors by your side.</h3>
 </div>
 <div className='exclusive'>
