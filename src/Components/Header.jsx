efc123785b08bac3db152d1708f6a2da6f7cba80
1995olayiwola <olayiwolarahmon84@gmail.com>
2024-07-07 04:56:26 -0700

    latest

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,92 +4,62 @@ import TopHeader from './TopHeader';
 import logo from './bidconnect.png';
 import {Link} from 'react-router-dom';
 import {FiAlignRight,FiXCircle,FiChevronDown } from "react-icons/fi";
-import { CSSTransition } from 'react-transition-group';
+
 
 
 
 const Header = () => {
 
-    
-  const [isMenu, setisMenu] = React.useState(false);
-  const [isResponsiveclose, setResponsiveclose] = useState(false);
-  const toggleClass = () => {
-    setisMenu(isMenu === false ? true : false);
-    setResponsiveclose(isResponsiveclose === false ? true : false);
-};
-
-  let boxClass = ["main-menu menu-right menuq1"];
-  if(isMenu) {
-      boxClass.push('menuq2');
-  }else{
-      boxClass.push('');
-  }
-
-  const [isMenuSubMenu, setMenuSubMenu] = useState(false);
-    
-  const toggleSubmenu = () => {
-    setMenuSubMenu(isMenuSubMenu === false ? true : false);
-  };
-  
-  let boxClassSubMenu = ["sub__menus"];
-  if(isMenuSubMenu) {
-      boxClassSubMenu.push('sub__menus__Active');
-  }else {
-      boxClassSubMenu.push('');
-  }
 
   return (
-    <header className="header__middle">
-        <div className="container_header">
-            <div className="row">
+<div>
+<nav className="navbar">
+  {/* LOGO */}
+  <div className="logo">
+    <img src={logo}  alt="Bidlist"/>
+  </div>
+  {/* NAVIGATION MENU */}
+  <ul className="nav-links">
+    {/* USING CHECKBOX HACK */}
+    <input type="checkbox" id="checkbox_toggle" />
+    <label htmlFor="checkbox_toggle" className="hamburger">
+      ☰
+    </label>
+    {/* NAVIGATION MENUS */}
+    <div className="menu">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li className="services">
+        <Link to="/bidlist">Bidlist</Link>
+        {/* DROPDOWN MENU */}
+        <ul className="dropdown">
+          <li>
+            <Link to="/uk">UK </Link>
+          </li>
+          <li>
+            <Link to="/us">US</Link>
+          </li>
+          <li>
+            <Link to="/canada">Canada</Link>
+          </li>
+         
+        </ul>
+      </li>
+      <li>
+        <Link to="/bidlist">Projects</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </div>
+  </ul>
+</nav>
 
-                {/* Add Logo  */}
-                <div className="header__middle__logo">
-                    <Link exact activeClassName='is-active' to="/">
-                    <img src={logo} alt="logo" /> 
-                    </Link>
-                </div>
-
-                <div className="header__middle__menus">
-              
-      
-                    <nav className="main-nav " >
-
-                    {/* Responsive Menu Button */}
-                    {isResponsiveclose === true ? <> 
-                        <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiXCircle />   </span>
-                    </> : <> 
-                        <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiAlignRight />   </span>
-                    </>}
-
-
-                    <ul className={boxClass.join(' ')}>
-                    <li  className="menu-item" >
-                        <Link exact activeClassName='is-active' onClick={toggleClass} to={`/`}> Home </Link> 
-                    </li>
-                    <li className="menu-item " ><Link onClick={toggleClass} activeClassName='is-active' to={`/About`}> About </Link> </li>
-                    <li onClick={toggleSubmenu} className="menu-item sub__menus__arrows" > <Link to="/bidlist"> Bidlist <FiChevronDown /> </Link>
-                        <ul className={boxClassSubMenu.join(' ')} > 
-                            <li> <Link onClick={toggleClass} activeClassName='is-active'  to={`/uk`}> UK </Link> </li>
-                            <li><Link onClick={toggleClass} activeClassName='is-active' to={`/us`}> US </Link> </li>
-                            <li><Link onClick={toggleClass} activeClassName='is-active' to={`/ca`}> Canada </Link> </li>
-                        </ul>
-                    </li>
-                    <li className="menu-item " ><Link onClick={toggleClass} activeClassName='is-active' to={`/Contact`}> Contact </Link> </li>
-                    <li className="menu-item " ><Link onClick={toggleClass} activeClassName='is-active' to={`/project`}> Projects </Link> </li>
-                    </ul>
-
-
-                    </nav>     
-                </div>   
-
-
-
-        
-        
-            </div>
-	    </div>
-    </header>
+</div>
   );
 };
 
